4531fe57b57fb76673c79228aac2a6514713ca01
Jose Eduardo Cury de Faria <jeduardocury@gmail.com>
2021-10-19 02:50:24 -0300

    incluido pets, api de serviço, upload de arquivo

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -2,10 +2,11 @@ class Tabelas {
     init(conexao){
         this.conexao =conexao
         this.criarAtendimentos()
+        this.criarPets()
     }
 
     criarAtendimentos(){
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimento (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY (id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimento (id int NOT NULL AUTO_INCREMENT, cliente varchar(11) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY (id))'
 
         this.conexao.query(sql, (erro) =>{
             if(erro){
@@ -14,6 +15,20 @@ class Tabelas {
                 console.log(`Tabela atendimentos criada com sucesso.`)
             }
         })
+    }
+
+    criarPets(){
+        const query =
+            'CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT, nome varchar(50), imagem varchar(200), PRIMARY KEY (id))'
+
+        this.conexao.query(query, erro => {
+            if(erro){
+                console.log(erro)
+            }else{
+                console.log('Tabela Pets criada com sucesso.')
+            }
+        })
+
     }
 }
 
